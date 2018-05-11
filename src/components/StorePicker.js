e17fd7c756085a701184ff9b583134a2505ac735
Theresa Shen <tshen@architech.ca>
2018-05-10 23:43:24 -0400

    Handle form onSubmit event, use ref to pass form data, use this.props.history.push() to change route

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  // handleClick() { alert("Hey!!") }
+  goToStore = (event) => { // Set as property not as a function
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from the input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page from / to /store/whaterver-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form onSubmit={this.goToStore} className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        {/* <button onClick={this.handleClick}>Click Me!</button> */}
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     )
